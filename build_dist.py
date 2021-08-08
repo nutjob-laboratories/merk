c51d50e66ecafee3a5cc2e9d95c16e180a0b3e76
Daniel Hetrick <dhetrick@gmail.com>
2021-08-08 04:37:33 -0400

    Added README system
    Added dynamic README system to the dist build system, and updated the README.md.

--- build_dist.py
+++ build_dist.py
@@ -38,6 +38,22 @@ if len(minor)==1:
 elif len(minor)==2:
 	minor = f"0{minor}"
 
+
+# Build README
+
+x = open("README.txt",mode="r", encoding='latin-1')
+readme = str(x.read())
+x.close()
+
+readme = readme.replace("!_VERSION_!",major)
+readme = readme.replace("!_MINOR_!",minor)
+readme = readme.replace("!_FULL_VERSION_!",major+"."+minor)
+
+os.remove("README.md")
+f = open("README.md",mode="w", encoding='latin-1')
+f.write(readme)
+f.close()
+
 # Build distribution zips
 
 os.mkdir("./dist")
