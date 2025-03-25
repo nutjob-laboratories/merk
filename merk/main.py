5f7aeaf270a6451c707d4af6899140006a0ff860
Daniel Hetrick <dhetrick@gmail.com>
2025-03-25 12:02:55 -0400

    Minor bugfix

--- merk/main.py
+++ merk/main.py
@@ -277,6 +277,29 @@ class Merk(QMainWindow):
 		entry = widgets.ExtendedMenuItemNoAction(self,APPLICATION_MENU_ICON,APPLICATION_NAME,APPLICATION_VERSION,25)
 		self.trayMenu.addAction(entry)
 
+		# self.trayConnect = QAction(QIcon(CONNECT_ICON),"Connect",self)
+		# self.trayConnect.triggered.connect(self.connectToIrc)
+		# self.trayMenu.addAction(self.trayConnect)
+
+		windows = self.getAllServerWindows()
+		clean = []
+		for w in windows:
+			c = w.widget()
+			if c.client.client_id in self.quitting: continue
+			clean.append(w)
+		windows = clean
+
+		# if self.is_hidden:
+		# 	self.trayConnect.setEnabled(False)
+
+		# if len(windows)>1:
+		# 	self.trayDisconnectAll = QAction(QIcon(DISCONNECT_ICON),"Disconnect all",self)
+		# 	self.trayDisconnectAll.triggered.connect(self.disconnectAll)
+		# 	self.trayMenu.addAction(self.trayDisconnectAll)
+
+		# self.trayMenu.addSeparator()
+
+
 		listOfConnections = {}
 		for i in irc.CONNECTIONS:
 			add_to_list = True
