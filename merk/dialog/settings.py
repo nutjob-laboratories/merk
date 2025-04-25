d1eb5aaf53486d235f3af3660ee9909a4e60b11d
Daniel Hetrick <dhetrick@gmail.com>
2025-04-25 13:25:40 -0400

    Tweak to settings dialog

--- merk/dialog/settings.py
+++ merk/dialog/settings.py
@@ -721,8 +721,8 @@ class Dialog(QDialog):
 			Dark mode changes the application palette to darker colors, which
 			is supposed to decrease eye strain. Text display colors are unchanged,
 			as those are set and controlled by the text style system.
-			<br><br><b>If dark mode
-			is enabled or disabled, the application must be restarted to use the
+			<br><br>
+			<b>If dark mode is enabled or disabled, the application must be restarted to use the
 			new application palette.</b>
 			</small>
 			<br>
