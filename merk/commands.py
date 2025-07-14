92fda69f0f69a2846374bbaa7580879433a75025
Daniel Hetrick <dhetrick@gmail.com>
2025-07-14 12:36:15 -0400

    Latest build

--- merk/commands.py
+++ merk/commands.py
@@ -2825,6 +2825,11 @@ class ScriptThread(QThread):
 
 	def run(self):
 
+		if not config.SCRIPTING_ENGINE_ENABLED:
+			self.scriptError.emit([self.gui,self.window,f"Scripting has been disabled"])
+			self.scriptEnd.emit([self.gui,self.id])
+			return
+
 		counter = 1
 		for a in self.arguments:
 			addTemporaryAlias(f"_{counter}",a)
