7975d2b9b79d222b61238185798ef4bc6ab725e3
Daniel Hetrick <dhetrick@gmail.com>
2025-09-05 19:23:04 -0400

    Dark palette

---
+++ dark.palette.py
@@ -0,0 +1,139 @@
+text {
+	color: #FFFFFF;
+}
+
+base {
+	color: #232323;
+}
+
+window {
+	color: #353535;
+}
+
+window_text {
+	color: #FFFFFF;
+}
+
+alternate_base {
+	color: #353535;
+}
+
+tooltip_base {
+	color: #191919;
+}
+
+tooltip_text {
+	color: #FFFFFF;
+}
+
+button {
+	color: #353535;
+}
+
+button_text {
+	color: #FFFFFF;
+}
+
+bright_text {
+	color: #FF0000;
+}
+
+link {
+	color: #2B82DA;
+}
+
+highlight {
+	color: #2B82DA;
+}
+
+highlighted_text {
+	color: #FFFFFF;
+}
+
+active_button {
+	color: #353535;
+}
+
+disabled_button_text {
+	color: #A9A9A9;
+}
+
+disabled_window_text {
+	color: #A9A9A9;
+}
+
+disabled_text {
+	color: #A9A9A9;
+}
+
+disabled_light {
+	color: #353535;
+}
+
+separator {
+	color: #A9A9A9;
+}
+"""
+
+DEFAULT_STYLE="""timestamp {
+	font-weight: bold;
+}
+
+username {
+	font-weight: bold;
+	color: #0000FF;
+}
+
+private {
+	font-style: italic;
+	font-weight: bold;
+	color: #0000FF;
+}
+
+message {
+}
+
+system {
+	font-weight: bold;
+	color: #FF8C00;
+}
+
+self {
+	font-weight: bold;
+	color: #FF0000;
+}
+
+action {
+	font-style: italic;
+	font-weight: bold;
+	color: #006400;
+}
+
+notice {
+	font-weight: bold;
+	color: #800080;
+}
+
+hyperlink {
+	text-decoration: underline;
+	font-weight: bold;
+	color: #0000FF;
+}
+
+all {
+	background-color: #FFFFFF;
+	color: #000000;
+}
+
+error {
+	font-weight: bold;
+	color: #FF0000;
+}
+
+server {
+	font-weight: bold;
+	color: #0073ad;
+}
+
+raw {
+}
