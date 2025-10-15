60c4a421dfbcf3b5d407cdd6877484ba5a93994e
Daniel Hetrick <dhetrick@gmail.com>
2025-10-15 12:15:59 -0400

    0.050.800 Release

--- merk/resources/release.py
+++ merk/resources/release.py
@@ -1,2 +1,2 @@
-APPLICATION_RELEASE = "https://github.com/nutjob-laboratories/merk/releases/tag/0.050.750"
-APPLICATION_RELEASE_VERSION = "0.050.750"
+APPLICATION_RELEASE = "https://github.com/nutjob-laboratories/merk/releases/tag/0.050.800"
+APPLICATION_RELEASE_VERSION = "0.050.800"
