31646a68d60518730382b3efe8d53015f834bd2a
Daniel Hetrick <dhetrick@gmail.com>
2026-08-03 20:33:52 -0400

    Bugfixes

--- merk/logs.py
+++ merk/logs.py
@@ -73,6 +73,7 @@ def initialize(directory,directory_name):
 
 # Functions
 
+# Encodes a filename to use to save a log backup
 def backup_filename(logfile):
 	if config.SHOW_TIMESTAMPS_IN_UTC:
 		ts = datetime.fromtimestamp(datetime.timestamp(datetime.now()),tz=timezone.utc).strftime('_%d_%m_%Y')
@@ -83,6 +84,7 @@ def backup_filename(logfile):
 	backup_name = backup_name + ts + ".txt"
 	return backup_name
 
+# Saves a backup of a single log file
 def backup_log_direct(logfile,outfile):
 	if os.path.isfile(logfile):
 		with open(logfile, "r",encoding="utf-8",errors="ignore") as logentries:
@@ -98,6 +100,7 @@ def backup_log_direct(logfile,outfile):
 		with open(logfile, "w",encoding="utf-8",errors="ignore") as writelog:
 			json.dump(store_log, writelog, indent=4, sort_keys=True)
 
+# Backups a list of log files, and saves the results to a directory
 def backup_log(logfile,directory):
 	if os.path.isfile(logfile):
 		with open(logfile, "r",encoding="utf-8",errors="ignore") as logentries:
@@ -114,6 +117,7 @@ def backup_log(logfile,directory):
 		with open(logfile, "w",encoding="utf-8",errors="ignore") as writelog:
 			json.dump(store_log, writelog, indent=4, sort_keys=True)
 
+# Searches for large log files, and returns a list of them
 def find_large_logs():
 	log_list = []
 	for root, dirs, files in os.walk(LOG_DIRECTORY):
@@ -141,6 +145,7 @@ def array_to_log(log):
 		out.append(m)
 	return out
 
+# Trims a log file down to a specific line count
 def trimLog(ilog,maxsize):
 	count = 0
 	shortlog = []
@@ -151,6 +156,7 @@ def trimLog(ilog,maxsize):
 			break
 	return list(reversed(shortlog))
 
+# Encodes a log name to use for storage
 def encodeLogName(network,name=None):
 	network = network.replace(":","-")
 	network = network.lower()
@@ -198,7 +204,8 @@ def readLog(network,name,logdir=LOG_DIRECTORY):
 	logs = array_to_log(logs)
 	return logs
 
-def pretty_timestamp_m1(ts):
+# Formats a timestamp for a date
+def pretty_timestamp_date(ts):
 	if config.TIMESTAMP_24_HOUR:
 		if config.SHOW_TIMESTAMPS_IN_UTC:
 			return datetime.fromtimestamp(ts,tz=timezone.utc).strftime('%a, %d %b %Y '+config.TIMESTAMP_FORMAT+' UTC')
@@ -210,7 +217,8 @@ def pretty_timestamp_m1(ts):
 		else:
 			return datetime.fromtimestamp(ts).strftime('%a, %d %b %Y '+config.TIMESTAMP_FORMAT+' %p')
 
-def pretty_timestamp_m2(ts):
+# Formats a timestamp for a time
+def pretty_timestamp_time(ts):
 	if config.TIMESTAMP_24_HOUR:
 		if config.SHOW_TIMESTAMPS_IN_UTC:
 			return datetime.fromtimestamp(ts,tz=timezone.utc).strftime(config.TIMESTAMP_FORMAT+' UTC')
@@ -221,21 +229,6 @@ def pretty_timestamp_m2(ts):
 			return datetime.fromtimestamp(ts,tz=timezone.utc).strftime(config.TIMESTAMP_FORMAT+' %p UTC')
 		else:
 			return datetime.fromtimestamp(ts).strftime(config.TIMESTAMP_FORMAT+' %p')
-
-def dumpRawLog(logs):
-	delimiter = "\t"
-	logs = log_to_array(logs)
-
-	out = []
-	for l in logs:
-		l[2] = l[2].strip()
-		l[3] = l[3].strip()
-		if l[2]=='': l[2] = '***'
-
-		pretty_timestamp = pretty_timestamp_m1(l[0])
-		entry = pretty_timestamp+delimiter+l[2]+delimiter+l[3]
-		if l[3]!="": out.append(entry)
-	return "\n".join(out)
 
 # Loads an AoA from disk, converts it to a string
 def dumpLog(filename,delimiter,linedelim="\n",epoch=True):
@@ -254,9 +247,10 @@ def dumpLog(filename,delimiter,linedelim="\n",epoch=True):
 			else:
 				l[3] = ''
 			if l[2]=='': l[2] = '***'
+			if l[2].startswith('&rarr;'): l[2] = l[2].replace('&rarr;','-> ',1)
 
 			if not epoch:
-				pretty_timestamp = pretty_timestamp_m1(l[0])
+				pretty_timestamp = pretty_timestamp_date(l[0])
 				entry = pretty_timestamp+delimiter+l[2]+delimiter+l[3]
 			else:
 				entry = str(l[0])+delimiter+l[2]+delimiter+l[3]
@@ -265,11 +259,15 @@ def dumpLog(filename,delimiter,linedelim="\n",epoch=True):
 	else:
 		return ''
 
+# Handles both rendering a log for the log viewer, and
+# exporting a log to the "human readable" format
 def dumpLogHuman(filename,render_for_viewer=False,epoch=False):
 	if os.path.isfile(filename):
 		with open(filename, "r",encoding="utf-8",errors="ignore") as logentries:
 			logs = json.load(logentries)
 
+	# If the setting is turned on, make sure that the log
+	# is "trimmed" for the log viewer
 	if config.LIMIT_LOG_VIEW:
 		if len(logs)>config.MAX_LOG_DISPLAY_SIZE and render_for_viewer==True:
 			logs = logs[-config.MAX_LOG_DISPLAY_SIZE:]
@@ -279,6 +277,7 @@ def dumpLogHuman(filename,render_for_viewer=False,epoch=False):
 	else:
 		trimmed = False
 
+	# Render the log
 	if logs:
 		out = []
 		cdate = None
@@ -299,97 +298,132 @@ def dumpLogHuman(filename,render_for_viewer=False,epoch=False):
 			else:
 				u = l[2]
 
-			if render_for_viewer==False: u = l[2]
+			# Just to make the code a bit more readable
+			username = u
+			message_type = l[1]
+			message = l[3]
+			timestamp = l[0]
 
+			# If we're not rendering the log for the log
+			# viewer, we display the entire nickname and
+			# hostmask
+			if render_for_viewer==False: username = l[2]
+
+			# Outgoing private messages are stored as
+			# "self" messages, with a HTML entity prefixed
+			# to the targeted user to show it's an outgoing
+			# private message; here, we replace the outgoing
+			# prefix HTML entity with an appropriate text
+			# representation
+			if message_type==SELF_MESSAGE:
+				if username.startswith('&rarr;'):
+					username = username.replace('&rarr;','-> ',1)
+
+			# Since the background of the log viewer is white,
+			# we need to make sure that white colored text is
+			# still visible; if we're rendering the log for the
+			# viewer, we'll replace white colored text with
+			# grey colored text
+			if render_for_viewer:
+				message = message.replace("\x0300","\x0314")
+
+			# If the log has been trimmed, it's being rendered
+			# for the log viewer, so notify the user that the
+			# log has been trimmed
 			if trimmed==True:
 				out.append(f"\x02\x1d\x0304Viewing only the last {len(logs)} lines of the log\x0f")
 				trimmed = False
 
+			# Now, we handle showing different dates.
+			# This will only show up when the date changes,
+			# so chat will be displayed/exported with notation
+			# showing what chat happened on what dates
 			if config.SHOW_TIMESTAMPS_IN_UTC:
-				ndate = datetime.fromtimestamp(l[0],tz=timezone.utc).strftime('%A %B %d, %Y UTC')
+				ndate = datetime.fromtimestamp(timestamp,tz=timezone.utc).strftime('%A %B %d, %Y UTC')
 			else:
-				ndate = datetime.fromtimestamp(l[0]).strftime('%A %B %d, %Y')
+				ndate = datetime.fromtimestamp(timestamp).strftime('%A %B %d, %Y')
 			if cdate!=ndate:
 				cdate=ndate
 				if render_for_viewer:
 					out.append(f"\x02\x1F{cdate}\x0f")
 				else:
 					if epoch:
-						out.append(f"*** {l[0]}")
+						out.append(f"*** {timestamp}")
 					else:
 						out.append(f"*** {cdate}")
 
-			if l[1]==CHAT_MESSAGE:
+			# Render chat messages according to message type
+			if message_type==CHAT_MESSAGE:
 				# Regular chat
 				if render_for_viewer:
-					l[3] = l[3].replace("\x0300","\x0301")
-					pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x0302{u}\x0f: {l[3]}"
+					pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x0302{username}\x0f: {message}"
 				else:
 					if epoch:
-						pretty_timestamp = l[0]
+						pretty_timestamp = timestamp
 					else:
-						pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"{pretty_timestamp}\t{u}: {strip_color(l[3])}"
-			elif l[1]==PRIVATE_MESSAGE:
+						pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"{pretty_timestamp}\t{username}: {strip_color(message)}"
+			elif message_type==PRIVATE_MESSAGE:
+				# Private message
+				if render_for_viewer:
+					pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x1d\x0302{username}\x0f: {message}"
+				else:
+					if epoch:
+						pretty_timestamp = timestamp
+					else:
+						pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"{pretty_timestamp}\t{username}: {strip_color(message)}"
+			elif message_type==SELF_MESSAGE:
 				# Regular chat
 				if render_for_viewer:
-					l[3] = l[3].replace("\x0300","\x0301")
-					pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x1d\x0302{u}\x0f: {l[3]}"
+					pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"\x02[{pretty_timestamp}]\x0f \x1F\x02\x0302{username}\x0f: {message}"
 				else:
 					if epoch:
-						pretty_timestamp = l[0]
+						pretty_timestamp = timestamp
 					else:
-						pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"{pretty_timestamp}\t{u}: {strip_color(l[3])}"
-			elif l[1]==SELF_MESSAGE:
-				# Regular chat
-				if render_for_viewer:
-					l[3] = l[3].replace("\x0300","\x0301")
-					pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"\x02[{pretty_timestamp}]\x0f \x1F\x02\x0302{u}\x0f: {l[3]}"
-				else:
-					if epoch:
-						pretty_timestamp = l[0]
-					else:
-						pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"{pretty_timestamp}\t{u}: {strip_color(l[3])}"
-			elif l[1]==ACTION_MESSAGE:
+						pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"{pretty_timestamp}\t{username}: {strip_color(message)}"
+			elif message_type==ACTION_MESSAGE:
 				# CTCP Action message
 				if render_for_viewer:
-					pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x1d\x0302{u} {l[3]}\x0f"
+					pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x1d\x0302{username} {message}\x0f"
 				else:
 					if epoch:
-						pretty_timestamp = l[0]
+						pretty_timestamp = timestamp
 					else:
-						pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"{pretty_timestamp}\t{u} {strip_color(l[3])}"
-			elif l[1]==NOTICE_MESSAGE:
+						pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"{pretty_timestamp}\t{username} {strip_color(message)}"
+			elif message_type==NOTICE_MESSAGE:
 				# Notice message
 				if render_for_viewer:
-					l[3] = l[3].replace("\x0300","\x0301")
-					pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x0302*{u}\x0f*: {l[3]}"
+					pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x0302*{username}\x0f*: {message}"
 				else:
 					if epoch:
-						pretty_timestamp = l[0]
+						pretty_timestamp = timestamp
 					else:
-						pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"{pretty_timestamp}\t*{u}*\t{strip_color(l[3])}"
+						pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"{pretty_timestamp}\t*{username}*\t{strip_color(message)}"
 			else:
+				# Everything else; this is usually logged system messages
 				if render_for_viewer:
-					pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x0304{l[3]}\x0f"
+					pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"\x02[{pretty_timestamp}]\x0f \x02\x0304{message}\x0f"
 				else:
 					if epoch:
-						pretty_timestamp = l[0]
+						pretty_timestamp = timestamp
 					else:
-						pretty_timestamp = pretty_timestamp_m2(l[0])
-					entry = f"{pretty_timestamp}\t{strip_color(l[3])}"
+						pretty_timestamp = pretty_timestamp_time(timestamp)
+					entry = f"{pretty_timestamp}\t{strip_color(message)}"
 
-			if l[3]!="": out.append(entry)
+			# If the message is not blank, add it to the
+			# log render/export
+			if message!="": out.append(entry)
+
 		return "\n".join(out)
 	else:
 		return ''
@@ -411,8 +445,9 @@ def dumpLogJson(filename,epoch=True):
 			else:
 				l[3] = ''
 			if l[2]=='': l[2] = '*'
+			if l[2].startswith('&rarr;'): l[2] = l[2].replace('&rarr;','-> ',1)
 			if not epoch:
-				l[0] = pretty_timestamp_m1(l[0])
+				l[0] = pretty_timestamp_date(l[0])
 			entry = [ l[0],l[2],l[3] ]
 			if l[3]!="": out.append(entry)
 		return json.dumps(out, indent=4, sort_keys=True)
